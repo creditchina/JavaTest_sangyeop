cd959c85e0db02257f167283d142e6273de3e7f3
creditchina <sangyeop.gip@gmail.com>
2017-03-22 16:06:10 +0900

    ver0.1

---
+++ HelloTest.java
@@ -0,0 +1,22 @@
+package main;
+
+public class HelloTest {
+
+	private String name;
+
+	public HelloTest(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+}
